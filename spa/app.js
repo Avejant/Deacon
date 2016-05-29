af191c2f2d8a8a063831097c0cb74fc33d879aa5
Avejant <avejant0@gmail.com>
2016-05-29 19:54:09 +0300

    Added i18n and l10n

--- spa/app.js
+++ spa/app.js
@@ -1,5 +1,23 @@
 'use strict'
-var app = angular.module('deaconApp', ['ngRoute', 'ngFileUpload','ui.bootstrap']).
+var app = angular.module('deaconApp', ['ngRoute', 'ngFileUpload', 'ui.bootstrap', 'ngCookies', 'pascalprecht.translate', 'tmh.dynamicLocale']).
+constant('LOCALES', {
+    'locales': {
+        'ru_RU': 'Русский',
+        'en_US': 'English'
+    },
+    'preferredLocale': 'en_US'
+}).
+config(function($translateProvider) {
+    $translateProvider.useStaticFilesLoader({
+        prefix: 'spa/resources/locale-', // path to translations files
+        suffix: '.json' // suffix, currently- extension of the translations
+    });
+    $translateProvider.preferredLanguage('en_US'); // is applied on first load
+    $translateProvider.useLocalStorage(); // saves selected language to localStorage
+}).
+config(function(tmhDynamicLocaleProvider) {
+    tmhDynamicLocaleProvider.localeLocationPattern('bower_components/angular-i18n/angular-locale_{{locale}}.js');
+}).
 config(['$routeProvider', function($routeProvider) {
     $routeProvider.
     when('/', {
@@ -38,13 +56,13 @@ config(['$routeProvider', function($routeProvider) {
         templateUrl: '/spa/partials/userList.html',
         controller: 'UserListCtrl'
     }).
-    when('/project/:id/backlog',{
-       templateUrl: '/spa/partials/backlog.html',
-       controller: 'backlogCtrl'
+    when('/project/:id/backlog', {
+        templateUrl: '/spa/partials/backlog.html',
+        controller: 'backlogCtrl'
     }).
-    when('/project/:id/activeSprint',{
-      templateUrl:'/spa/partials/activeSprint.html',
-      controller: 'activeSprintCtrl'
+    when('/project/:id/activeSprint', {
+        templateUrl: '/spa/partials/activeSprint.html',
+        controller: 'activeSprintCtrl'
     }).
     otherwise({
         redirectTo: '/'
---
+++ spa/directives/LanguageSelectDirective.js
@@ -0,0 +1,31 @@
+angular.module('deaconApp').directive('ngTranslateLanguageSelect', function(LocaleService) {
+    'use strict';
+    return {
+        restrict: 'A',
+        replace: true,
+        template: '' +
+            '<li class="language-select dropdown" ng-if="visible">' +
+            '<a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{"directives.language-select.Language" | translate}} <span class="caret"></span></a>'+
+            '<ul class="dropdown-menu">'+
+            '<li ' +
+            'ng-repeat="localesDisplayName in localesDisplayNames"' +
+            'ng-click="changeLanguage(localesDisplayName)">' +
+            '<a href=""> {{localesDisplayName}} <span ng-if="localesDisplayName === currentLocaleDisplayName" class="glyphicon glyphicon-ok"></span></a>'+
+            '</li>' +
+            '</ul>'+
+            '</li>' +
+            '',
+        controller: function($scope) {
+            //$scope.currentLocaleDisplayName = LocaleService.getLocaleDisplayName();
+            $scope.currentLocaleDisplayName = 'English';
+            $scope.localesDisplayNames = LocaleService.getLocalesDisplayNames();
+            $scope.visible = $scope.localesDisplayNames &&
+                $scope.localesDisplayNames.length > 1;
+
+            $scope.changeLanguage = function(locale) {
+                LocaleService.setLocaleByDisplayName(locale);
+                $scope.currentLocaleDisplayName = locale;
+            };
+        }
+    };
+});
